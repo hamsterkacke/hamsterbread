f4f18c40d5be30e01cc99f5eaa7400afa3b7f764
Philipp Noack <nophtu@gmail.com>
2011-04-26 01:00:20 +0200

    initial push

---
+++ hamsterbread.js
@@ -0,0 +1,10 @@
+{
+    "developer": "Hamsterbread",
+    "free": "true",
+    "id": "hamsterbread",
+    "manifest": "https://github.com/Hamsterkacke/Hamsterbread-Rom-Manager-Stuff/blob/master/manifest.js",
+    "roms": {
+        "galaxys": "true" 
+    },
+    "summary": "Some Hamster Love for your phone!" 
+}
---
+++ manifest.js
@@ -0,0 +1,25 @@
+{
+    "version": "1",
+    "homepage": "http://forum.xda-developers.com/showthread.php?t=844514",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Q874QG28TAG5U",
+    "roms":
+    [
+    {
+        "name": "Hamsterbread 1.0",
+        "summary": "Stable",
+        "device": "galaxys",
+        "product": "Hamsterbread",
+        "incremental": "1000",
+        "modversion": "Hamsterbread-1.0",
+        "screenshots":
+        [
+        "http://img200.imageshack.us/img200/3009/image001ih.png",
+        "http://img705.imageshack.us/img705/6954/image002hn.png"
+        ],
+        "urls":
+        [
+        "http://dl.dropbox.com/u/10693833/Hamsters%20Rom/Hamsterbread1.0.zip"
+        ]
+    }
+	]
+}
