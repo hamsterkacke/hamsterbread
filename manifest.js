a25fffd6bff805a4abd30ab82186b00098ab0e4f
Hamsterkacke <nophtu@gmail.com>
2011-04-26 03:46:12 -0700

    Edited manifest.js via GitHub

--- manifest.js
+++ manifest.js
@@ -1,38 +1,42 @@
 {
-	version: 1,
-	homepage: "http://forum.xda-developers.com/showthread.php?t=844514",
-	donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=94XVZG4T7JDY4",
-	roms:
-	[
-	{
-		name: "Hamsterbread 1.0",
-		summary: "Full wipe image, this will wipe your phone.",
-		device: "galaxys",
-		product: "Hamsterbread",
-		screenshots:
-		[
-		"http://img200.imageshack.us/img200/3009/image001ih.png",
-		"http://img705.imageshack.us/img705/6954/image002hn.png"
-		],
-		url: "http://dl.dropbox.com/u210693833/Hamsters%20Rom/Hamsterbread1.0.zip"
-	},
-	{
-		name: "Hamsterbread 1.1",
-		summary: "Insanity Upgrade Package.",
-		device: "galaxys",
-		product: "Hamsterbread",
-		incremental: 0011,
-		modversion: "Hamsterbread-1.1",
-		url: "http://dl.dropbox.com/"
-	},
-	{
-		name: "Hamsterbread 1.0",
-		visible: "false",
-		device: "galaxys",
-		product: "Hamsterbread",
-		incremental: 0010,
-		modversion: "Hamsterbread-1.0",
-		url: ""
-	}
+    "version": "1",
+    "homepage": "http://forum.xda-developers.com/showthread.php?t=844514",
+    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Q874QG28TAG5U",
+    "roms":
+    [
+ {
+        "name": "Test",
+        "summary": "test",
+        "device": "galaxys",
+        "product": "Hamsterbread",
+        "incremental": "1010",
+        "modversion": "Hamsterbread-1.1",
+        "screenshots":
+        [
+        "http://img200.imageshack.us/img200/3009/image001ih.png",
+        "http://img705.imageshack.us/img705/6954/image002hn.png"
+        ],
+        "urls":
+        [
+        ""
+        ]
+    },
+    {
+        "name": "Hamsterbread 1.0",
+        "summary": "Stable",
+        "device": "galaxys",
+        "product": "Hamsterbread",
+        "incremental": "1000",
+        "modversion": "Hamsterbread-1.0",
+        "screenshots":
+        [
+        "http://img200.imageshack.us/img200/3009/image001ih.png",
+        "http://img705.imageshack.us/img705/6954/image002hn.png"
+        ],
+        "urls":
+        [
+        "http://dl.dropbox.com/u/10693833/Hamsters%20Rom/Hamsterbread1.0.zip"
+        ]
+    }
 	]
 }
