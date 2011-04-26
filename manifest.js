7a8b3ef472db219290666e4d24b113735b393561
nitr8 <nitr8@nfye.com>
2011-04-26 03:17:39 -0700

    Edited manifest.js via GitHub

--- manifest.js
+++ manifest.js
@@ -1,42 +1,38 @@
 {
-    "version": "1",
-    "homepage": "http://forum.xda-developers.com/showthread.php?t=844514",
-    "donate": "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=Q874QG28TAG5U",
-    "roms":
-    [
-    {
-        "name": "Test",
-        "summary": "test",
-        "device": "galaxys",
-        "product": "Test",
-        "modversion": "Hamsterbread-1.1",
-        "incremental": "0011",
-        "screenshots":
-        [
-        "http://img200.imageshack.us/img200/3009/image001ih.png",
-        "http://img705.imageshack.us/img705/6954/image002hn.png"
-        ],
-        "urls":
-        [
-        "http://dl.dropbox.com/u210693833/Hamsters%20Rom/Hamsterbread1.0.zip"
-        ]
-    },
-    {
-        "name": "Hamsterbread 1.0",
-        "summary": "Stable",
-        "device": "galaxys",
-        "product": "Hamsterbread",
-        "modversion": "Hamsterbread-1.0",
-        "incremental": "0010",
-        "screenshots":
-        [
-        "http://img200.imageshack.us/img200/3009/image001ih.png",
-        "http://img705.imageshack.us/img705/6954/image002hn.png"
-        ],
-        "urls":
-        [
-        "http://dl.dropbox.com/u/10693833/Hamsters%20Rom/Hamsterbread1.0.zip"
-        ]
-    }
+	version: 1,
+	homepage: "http://forum.xda-developers.com/showthread.php?t=844514",
+	donate: "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=94XVZG4T7JDY4",
+	roms:
+	[
+	{
+		name: "Hamsterbread 1.0",
+		summary: "Full wipe image, this will wipe your phone.",
+		device: "galaxys",
+		product: "Hamsterbread",
+		screenshots:
+		[
+		"http://img200.imageshack.us/img200/3009/image001ih.png",
+		"http://img705.imageshack.us/img705/6954/image002hn.png"
+		],
+		url: "http://dl.dropbox.com/u210693833/Hamsters%20Rom/Hamsterbread1.0.zip"
+	},
+	{
+		name: "Hamsterbread 1.0",
+		summary: "Insanity Upgrade Package.",
+		device: "galaxys",
+		product: "Hamsterbread",
+		incremental: 0011,
+		modversion: "insanity-7.2",
+		url: "http://dl.dropbox.com/"
+	},
+	{
+		name: "Insanity 0.7.1",
+		visible: "false",
+		device: "galaxys",
+		product: "Insanity",
+		incremental: 0010,
+		modversion: "insanity-7.1",
+		url: ""
+	}
 	]
 }
